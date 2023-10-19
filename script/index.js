62f3de010b7831616eb9cde6891189ab69a990ea
renan.theodoro <renan.b.theodoro@gmail.com>
2023-10-19 10:06:09 -0300

    fix: correct layout issues

--- script/index.js
+++ script/index.js
@@ -18,11 +18,40 @@ qrCodeBox.addEventListener('click', () => {
 const video = document.getElementById("video");
 const playButton = document.getElementById("play-button");
 const mediaWrapper = document.getElementById("media-wrapper");
+video.controls = false;
+
+function requestFullscreen() {
+    // if (video.requestFullscreen) {
+    //     video.requestFullscreen();
+    // } else if (video.mozRequestFullScreen) { // Firefox
+    //     video.mozRequestFullScreen();
+    // } else if (video.webkitRequestFullscreen) { // Chrome, Safari e Opera
+    //     video.webkitRequestFullscreen();
+    // } else if (video.msRequestFullscreen) { // Internet Explorer e Edge
+    //     video.msRequestFullscreen();
+    // }
+    mediaWrapper.classList.add("view--cases-detail__media--video-fullscreen");
+}
+
+function exitFullscreen() {
+    // if (document.exitFullscreen) {
+    //     document.exitFullscreen();
+    // } else if (document.mozCancelFullScreen) { // Firefox
+    //     document.mozCancelFullScreen();
+    // } else if (document.webkitExitFullscreen) { // Chrome, Safari, e Opera
+    //     document.webkitExitFullscreen();
+    // } else if (document.msExitFullscreen) { // Internet Explorer e Edge
+    //     document.msExitFullscreen();
+    // }
+
+    mediaWrapper.classList.remove("view--cases-detail__media--video-fullscreen");
+}
 
 playButton.addEventListener("click", function () {
     if (video.paused) {
         video.play();
         mediaWrapper.classList.add("view--cases-detail__media--video-played");
+        requestFullscreen();
     } else {
         video.pause();
         mediaWrapper.classList.remove("view--cases-detail__media--video-played");
@@ -30,20 +59,15 @@ playButton.addEventListener("click", function () {
 });
 
 video.addEventListener("ended", function () {
-    // O evento "ended" é acionado quando o vídeo chega ao fim.
     mediaWrapper.classList.remove("view--cases-detail__media--video-played");
 });
 
 const fullscreenButton = document.getElementById("fullscreen-button");
 
 fullscreenButton.addEventListener("click", function () {
-    if (video.requestFullscreen) {
-        video.requestFullscreen();
-    } else if (video.mozRequestFullScreen) { // Firefox
-        video.mozRequestFullScreen();
-    } else if (video.webkitRequestFullscreen) { // Chrome, Safari e Opera
-        video.webkitRequestFullscreen();
-    } else if (video.msRequestFullscreen) { // Internet Explorer e Edge
-        video.msRequestFullscreen();
+    if (mediaWrapper.classList.contains("view--cases-detail__media--video-fullscreen")) {
+        exitFullscreen();
+    } else {
+        requestFullscreen();
     }
 });
